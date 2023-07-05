0f704a8aa6ddbaedc1862d6407081531ca7b9389
mehedy hassan <mehedy@algosolver.com>
2023-07-06 01:28:50 +0600

    global loader css added

--- components/reusable/Loader/index.jsx
+++ components/reusable/Loader/index.jsx
@@ -2,7 +2,7 @@ import { HashLoader } from "react-spinners";
 
 const Loader = ({ className }) => {
   return (
-    <div className={`${className}`}>
+    <div className={`${className ?? 'bg-gray-200 h-screen w-full z-9999 flex items-center justify-center' }`}>
       <HashLoader color="#11b981" />
     </div>
   );
