2b446d3193517c895c035d1a5f8e6e527ddcb541
mehedy hassan <mehedy@algosolver.com>
2023-07-06 01:27:23 +0600

    loader globally added

--- components/reusable/Loader/index.jsx
+++ components/reusable/Loader/index.jsx
@@ -2,8 +2,7 @@ import { HashLoader } from "react-spinners";
 
 const Loader = ({ className }) => {
   return (
-    <div
-      className={`${className}`}>
+    <div className={`${className}`}>
       <HashLoader color="#11b981" />
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,27 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Router } from "next/router";
+import { useEffect, useState } from "react";
+import Loader from "../components/reusable/Loader";
 import "../styles/globals.css";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
+  const [globalLoader, setGlobalLoader] = useState(false);
+
+  useEffect(() => {
+    Router.events.on("routeChangeStart", (as, { shallow }) => {
+      if (!shallow) {
+        setGlobalLoader(true);
+      }
+    });
+    Router.events.on("routeChangeComplete", () => setGlobalLoader(false));
+    Router.events.on("routeChangeError", () => setGlobalLoader(false));
+  }, []);
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        {globalLoader && <Loader />}
         <Component {...pageProps} />
       </QueryClientProvider>
     </>
