1027d26913f43e32d29bb7553cdaeb5ba3d4463a
mehedy hassan <mehedy@algosolver.com>
2023-08-01 02:47:57 +0600

    user id aded

--- components/pages/user/Edit/index.jsx
+++ components/pages/user/Edit/index.jsx
@@ -23,6 +23,7 @@ const EditUser = ({ userItem, state, setState, pswIsLoading }) => {
 
   useEffect(() => {
     setValue("userName", userItem?.userName);
+    setValue("userId", userItem?.id);
     setValue("name", userItem?.name);
     setValue("role", userItem?.role);
   }, [userItem]);
@@ -57,6 +58,10 @@ const EditUser = ({ userItem, state, setState, pswIsLoading }) => {
                     {...register("userName")}
                     required
                     className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary-600 shadow-sm rounded-lg"
+                  />
+                  <input
+                    type="hidden"
+                    {...register("userId", { required: true })}
                   />
                 </fieldset>
               </div>
